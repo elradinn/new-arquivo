1d0dd641618f4e2e5d657b06c743dc117ca38dda
elradinn <johnalfredalfonso.dev@gmail.com>
2024-11-02 05:11:13 +0800

    gmail testing, check env

--- routes/mock_main.php
+++ routes/mock_main.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Mail;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -15,4 +16,13 @@ Route::get('/', function () {
 
 Route::get('/api-tester', function () {
     return Inertia::render('ApiTester');
+});
+
+Route::get('/send-test-email', function () {
+    Mail::raw('This is a test email from Laravel!', function ($message) {
+        $message->to('serenityshoshin@gmail.com')
+            ->subject('Test Email');
+    });
+
+    return 'Test email sent!';
 });
